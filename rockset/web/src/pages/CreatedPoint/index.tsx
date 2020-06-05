ae9e5397f50a47ea8777725e5211f4c5bc27aa05
andressalh <andressalhenriques@hotmail.com>
2020-06-05 20:31:32 -0300

    update

--- rockset/web/src/pages/CreatedPoint/index.tsx
+++ rockset/web/src/pages/CreatedPoint/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import { FiCheckCircle} from 'react-icons/fi';
+import { FiCheckCircle, FiArrowLeft} from 'react-icons/fi';
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 const CreatedPoint = () => {
     return (
         <div id="page-created-point">
+            <header>
+                <Link to="/">
+                    <span>
+                        <FiArrowLeft/>
+                    </span>
+                    <strong>Voltar</strong>
+                    </Link>
+            </header>
             <div className="content-created-point">
                 <main>
                     <FiCheckCircle/>
-                    <h1>Cadastro concluído!</h1>
+                    <h1>Cadastro concluído!</h1>           
                 </main>
             </div>
+            
+            
         </div>
     )
 }
